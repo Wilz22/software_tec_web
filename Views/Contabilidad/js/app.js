7d1efb332ba308ef72790e03d9b1a4cf43543820
ags-mites <eamites1@espe.edu.ec>
2024-01-13 00:15:34 -0500

    actualizacion de JS

--- Views/Contabilidad/js/app.js
+++ Views/Contabilidad/js/app.js
@@ -4,3 +4,37 @@ btnfilter.addEventListener("click", () => {
     document.querySelector(".filter-menu").classList.toggle("active")
 }); 
 
+
+const add_item = document.querySelector('.add-item');
+const modal = document.querySelector('.modal');
+
+add_item.addEventListener('pointerdown', () => {
+    modal.style.display = 'block';
+    setTimeout(() => modal.classList.add('show'), 0)
+});
+
+modal.querySelector('.close').addEventListener('pointerdown', () => {
+    hideModal();
+});
+
+modal.querySelector('.cancel').addEventListener('pointerdown', () => {
+    hideModal();
+});
+
+document.addEventListener('pointerdown', (e) => {
+    if (!e.composedPath().includes(modal)) {
+        hideModal();  
+    }
+});
+
+modal.addEventListener('transitionend', function(e) {
+    if (!this.classList.contains('show')) {
+        if (e.propertyName == 'transform') {
+        this.style.display = '';
+        }
+    }
+});
+
+function hideModal() {
+    modal.classList.remove('show')
+}
