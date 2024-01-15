63d5e2a8c93dfeb058f0de07c5537e548645f62d
MilenaReyes05 <somoscuatro1980@gmail.com>
2024-01-14 21:47:13 -0500

    beta18

--- assets/Nomina/js/DetalleNomina.js
+++ assets/Nomina/js/DetalleNomina.js
@@ -9,20 +9,20 @@ document.getElementById('Insertar').addEventListener('click',function(){
 })
 document.getElementById('Eliminar').addEventListener('click',function(){
     alert('El contenido ha sido Eliminado' )
-})
+})*/
 document.getElementById('Asiento Contable').addEventListener('click',function(){
     alert('Se a realizado el asiento contable' )
-})*/
+
+})
 function limpiartodo(){
                 
-    document.getElementById('cedula').value= "";
-    document.getElementById('nombres').value = "";
-    document.getElementById('apellidos').value = "";
-    document.getElementById('mote').value= "";
-    document.getElementById('prueba').value = "";
+    document.getElementById('numero').value= "";
+    document.getElementById('fecha').value = "";
+    document.getElementById('empleado').value = "";
+
 }
         document.addEventListener('DOMContentLoaded', function () {
-        const insertarClienteBtn = document.getElementById('Insertar');
+        const insertarClienteBtn = document.getElementById('Insert');
         // const cedulaInput = document.querySelector('.campo:nth-child(1) input');
         // const NombresInput = document.querySelector('.campo:nth-child(2) input');
         // const ApellidosInput = document.querySelector('.campo:nth-child(3) input');
@@ -37,54 +37,50 @@ function limpiartodo(){
         const clientesData = [];
 
         insertarClienteBtn.addEventListener('click', function () {
-            const cedula= document.getElementById('cedula').value;
-            const Nombres= document.getElementById('nombres').value;
-            const Apellidos= document.getElementById('apellidos').value;
-            const Motivo= document.getElementById('mote').value;
-            const Detalle= document.getElementById('prueba').value;
-            console.log(cedula);
-            console.log(Nombres);
-            console.log(Apellidos);
-            console.log(Motivo);
+            const Numero= document.getElementById('numero').value;
+            const Fecha= document.getElementById('fecha').value;
+            const Empleado= document.getElementById('empleado').value;
+            console.log(Numero);
+            console.log(Fecha);
+            console.log(Empleado);
+           // console.log(Motivo);
             //console.log(DetalleInputii);
-            console.log(Detalle);
+           // console.log(Detalle);
             // const valor= valorInput.value;
            // var valorInput = document.getElementById('valorNumerico').value;
            // var valorNum=parseFloat(valorInput);
             //console.log(valorNum);
             // Validaciones
-            if (cedula.length !== 10) {
-                showMessage('La Cédula debe tener 10 dígitos.', 'red');
+            if (Numero.length !== 10) {
+                showMessage('El número debe tener 10 dígitos.', 'red');
                 return;
             }
 
-            if (Nombres.length > 25) {
+            if (Fecha.length > 25) {
                 showMessage('Los Nombres no pueden exceder los 25 caracteres.', 'red');
                 return;
             }
 
     
-            if (Apellidos.length > 30) {
-                showMessage('La Apellidos no pueden exceder los 30 caracteres.', 'red');
+            if (Empleado.length > 30) {
+                showMessage('Los nombres del empleado no pueden exceder los 30 caracteres.', 'red');
                 return;
             }
-            if (Motivo.length > 30){
+          /*  if (Motivo.length > 30){
                 showMessage('El Motivo no puede exceder de 30 carácteres.', 'red');
                 return;
             }
             if (Detalle.length > 30){
                 showMessage('El Detalle no puede exceder de 30 carácteres.', 'red');
                 return;
-            }
-            if (cedula && Nombres && Apellidos && Motivo && Detalle) {
+            }*/
+            if (Numero && Fecha && Empleado ) {
                 // Crear un objeto con los datos del cliente
                 const cliente = {
                     codigo: clientesData.length + 1,
-                    cedula: cedula,
-                    Nombres: Nombres,
-                    Apellidos: Apellidos,
-                    Motivo:Motivo,
-                    Detalle:Detalle,
+                    Numero: Numero,
+                    Fecha: Fecha,
+                    Empleado: Empleado,
                     conf: 'Modificar'
                     
                 };
@@ -110,11 +106,9 @@ function limpiartodo(){
             clientesData.forEach(function (cliente) {
                 const row = document.createElement('tr');
                 row.innerHTML = `
-                    <td>${cliente.cedula}</td>
-                    <td>${cliente.Nombres}</td>
-                    <td>${cliente.Apellidos}</td>
-                    <td>${cliente.Motivo}</td>
-                    <td>${cliente.Detalle}</td>
+                    <td>${cliente.Telefono}</td>
+                    <td>${cliente.Fecha}</td>
+                    <td>${cliente.Empleado}</td>
                     <td class="element__modifier">
                     <button class="botonMod botonMod-active" onclick="mostrarFormularioEdicion(${cliente.codigo})">${cliente.conf}</button>
                     </td>
@@ -124,7 +118,7 @@ function limpiartodo(){
             });
         }
 
-       const EliminarBtn = document.getElementById('Eliminar');
+       const EliminarBtn = document.getElementById('Delete');
     EliminarBtn.addEventListener('click', function () {
         eliminarPrimeraFila();
     });
@@ -163,11 +157,9 @@ function limpiartodo(){
         const cliente = clientesData.find(cliente => cliente.codigo === codigo);
         // const cod = clientesData.find(cod=> cod.codigo === codigo); 
         console.log(cliente);
-        document.getElementById('cedula').value = cliente.cedula;
-        document.getElementById('nombres').value = cliente.Nombres;
-        document.getElementById('apellidos').value = cliente.Apellidos;
-        document.getElementById('mote').value = cliente.Motivo;
-        document.getElementById('prueba').value = cliente.Detalle;
+        document.getElementById('numero').value = cliente.Numero;
+        document.getElementById('fecha').value = cliente.Fecha;
+        document.getElementById('empleado').value = cliente.Empleado;
 
         // const tableBody = document.querySelector('.body__table');
         // document.getElementById('valornumber').value = cliente.valor;
@@ -186,15 +178,13 @@ function limpiartodo(){
         //     clientesData[index].direccion = nuevaDireccion;
         //     clientesData[index].edad = nuevaEdad;
         // }
-        let btnGuardarcamb= document.getElementById('Guardar');
+        let btnGuardarcamb= document.getElementById('Keep');
 
         btnGuardarcamb.onclick=function(){
 
-        cliente.cedula = document.getElementById('cedula').value 
-        cliente.Nombres = document.getElementById('nombres').value 
-        cliente.Apellidos =document.getElementById('apellidos').value 
-        cliente.Motivo = document.getElementById('mote').value
-        cliente.Detalle =document.getElementById('prueba').value 
+        cliente.Telefono = document.getElementById('telefono').value 
+        cliente.Fecha = document.getElementById('fecha').value 
+        cliente.Empleado =document.getElementById('empleado').value 
         
         updateTable();
         showMessage('Se ha modificado la fila correctamente', 'blue');
@@ -215,5 +205,199 @@ function limpiartodo(){
     }
     });
     
+//////////////////////////////////////////////////////////////////////////////////////
 
-
+    function limpiartodo(){
+                
+        document.getElementById('ingreso').value= "";
+        document.getElementById('egreso').value = "";
+        document.getElementById('valor').value = "";
+    
+    }
+            document.addEventListener('DOMContentLoaded', function () {
+            const insertarClienteBtn = document.getElementById('Insertar');
+            // const cedulaInput = document.querySelector('.campo:nth-child(1) input');
+            // const NombresInput = document.querySelector('.campo:nth-child(2) input');
+            // const ApellidosInput = document.querySelector('.campo:nth-child(3) input');
+            //     //var valorInput = document.getElementById('valornumber').value;
+            // // const valorInput = document.querySelector('.campo:nth-child(4) input')
+            // const MotivoInput = document.querySelector('.campo:nth-child(4) input'); //
+            // const DetalleInput = document.querySelector('.campo:nth-child(4) input'); //
+            
+    
+            const tableBody = document.querySelector('.cuerpo_tabla');
+            //var valorNum=parseFloat(valorInput);
+            const clientesData = [];
+    
+            insertarClienteBtn.addEventListener('click', function () {
+                const Ingreso= document.getElementById('ingreso').value;
+                const Egreso= document.getElementById('egreso').value;
+                const Valor= document.getElementById('valor').value;
+                console.log(Ingreso);
+                console.log(Egreso);
+                console.log(Valor);
+               // console.log(Motivo);
+                //console.log(DetalleInputii);
+               // console.log(Detalle);
+                // const valor= valorInput.value;
+               // var valorInput = document.getElementById('valorNumerico').value;
+               // var valorNum=parseFloat(valorInput);
+                //console.log(valorNum);
+                // Validaciones
+                if (Ingreso.length > 25) {
+                    showMessage('El Ingreso no pueden exceder los 25 caracteres.', 'red');
+                    return;
+                }
+    
+                if (Egreso.length > 25) {
+                    showMessage('El Egreso no pueden exceder los 25 caracteres.', 'red');
+                    return;
+                }
+    
+        
+                if (Valor.length !== 5) {
+                    showMessage('El valor debe tener 5 dígitos.', 'red');
+                    return;
+                }
+              /*  if (Motivo.length > 30){
+                    showMessage('El Motivo no puede exceder de 30 carácteres.', 'red');
+                    return;
+                }
+                if (Detalle.length > 30){
+                    showMessage('El Detalle no puede exceder de 30 carácteres.', 'red');
+                    return;
+                }*/
+                if ( Ingreso && Egreso && Valor ) {
+                    // Crear un objeto con los datos del cliente
+                    const cliente = {
+                        codigo: clientesData.length + 1,
+                        Ingreso: Ingreso,
+                        Egreso: Egreso,
+                        Valor: Valor,
+                        conf: 'Modificar'
+                        
+                    };
+        
+                    // Agregar el cliente al array
+                    clientesData.push(cliente);
+        
+                    // Actualizar la tabla
+                    updateTable();
+                    showMessage('Se ha insertado correctamente el motivo', 'green');
+                    limpiartodo();
+        
+                } else {
+                    alert('Completa todos los campos antes de agregar un motivo.');
+                }
+            });
+        
+            function updateTable() {
+            // Limpiar el cuerpo de la tabla
+            tableBody.innerHTML = '';
+        
+            // Rellenar la tabla con los datos de los clientes
+                clientesData.forEach(function (cliente) {
+                    const row = document.createElement('tr');
+                    row.innerHTML = `
+                        <td>${cliente.Ingreso}</td>
+                        <td>${cliente.Egreso}</td>
+                        <td>${cliente.Valor}</td>
+                        <td class="element__modifier">
+                        <button class="botonMod botonMod-active" onclick="mostrarFormularioEdicion(${cliente.codigo})">${cliente.conf}</button>
+                        </td>
+                    `;
+        
+                    tableBody.appendChild(row);
+                });
+            }
+    
+           const EliminarBtn = document.getElementById('Eliminar');
+        EliminarBtn.addEventListener('click', function () {
+            eliminarPrimeraFila();
+        });
+        function eliminarPrimeraFila() {
+            if (clientesData.length > 0) {
+                // Elimina el primer elemento del array
+                clientesData.shift();
+    
+                // Actualiza la tabla
+                updateTable();
+                showMessage('Se ha eliminado la primera fila correctamente', 'green');
+    
+            } else {
+                showMessage('No hay filas para eliminar', 'red');
+            }
+        }
+    
+    
+        // tableBody.addEventListener('click', function (event) {
+        //     const target = event.target;
+        //     if (target.classList.contains('botonMod')) {
+        //         const row = target.closest('tr');
+        //         const codigo = row.children[0].textContent;
+        //         const nombre = row.children[1].textContent;
+        //         console.log(nombre);
+        //         console.log(codigo);
+        //         const cedula = row.children[2].textContent;
+        //         const direccion = row.children[3].textContent;
+        //         const edad = row.children[4].textContent;
+                
+        //         mostrarFormularioEdicion(codigo);
+        //     }
+        // });
+    
+        window.mostrarFormularioEdicion = function(codigo) {
+            const cliente = clientesData.find(cliente => cliente.codigo === codigo);
+            // const cod = clientesData.find(cod=> cod.codigo === codigo); 
+            console.log(cliente);
+            document.getElementById('ingreso').value = cliente.Ingreso;
+            document.getElementById('egreso').value = cliente.Egreso;
+            document.getElementById('valor').value = cliente.Valor;
+    
+            // const tableBody = document.querySelector('.body__table');
+            // document.getElementById('valornumber').value = cliente.valor;
+            // const nuevoNombre = prompt('Editar Nombre:', nombre);
+            // const nuevaCedula = prompt('Editar Cédula:', cedula);
+            // const nuevaDireccion = prompt('Editar Dirección:', direccion);
+            // const nuevaEdad = prompt('Editar Edad:', edad);
+    
+    
+    
+            // Actualizar los datos en el array
+            // const index = clientesData.findIndex(cliente => cliente.codigo === parseInt(codigo));
+            // if (index !== -1) {
+            //     clientesData[index].nombre = nuevoNombre;
+            //     clientesData[index].cedula = nuevaCedula;
+            //     clientesData[index].direccion = nuevaDireccion;
+            //     clientesData[index].edad = nuevaEdad;
+            // }
+            let btnGuardarcamb= document.getElementById('Guardar');
+    
+            btnGuardarcamb.onclick=function(){
+    
+            cliente.Ingreso = document.getElementById('ingreso').value 
+            cliente.Egreso = document.getElementById('egreso').value 
+            cliente.Valor =document.getElementById('valor').value 
+            
+            updateTable();
+            showMessage('Se ha modificado la fila correctamente', 'blue');
+            limpiartodo();
+            }
+                    // Actualizar la tabla 
+        }
+    
+        function showMessage(message, backgroundColor) {
+            const messageContainer = document.createElement('div');
+            messageContainer.classList.add('message-container');
+            messageContainer.style.backgroundColor = backgroundColor;
+            messageContainer.textContent = message;
+            document.body.appendChild(messageContainer);
+            setTimeout(() => {
+                document.body.removeChild(messageContainer);
+            }, 3000);
+        }
+        });
+        
+    
+    
+    
